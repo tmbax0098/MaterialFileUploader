dd7d455da3e69aa63b09a9d6eb1165a3a4b7710c
amelsystemco@outlook.com <kooroshrad9898@gmail.com>
2021-03-24 17:59:31 +0430

    version 0.0.9

--- dist/FileUploader.d.ts
+++ dist/FileUploader.d.ts
@@ -1,33 +1,44 @@
 /// <reference types="react" />
 import * as PropTypes from "prop-types";
 interface FileUploaderProps {
-    borderWidth?: number;
-    max?: number;
-    title?: string;
-    chooseText?: "Choose";
-    deleteIcon?: any;
-    className?: any;
-    emptyGuideText?: "Choose file!";
+    borderWidth: number;
+    maxCount: number;
+    minCount: number;
+    title: string;
+    chooseText: "Choose";
+    deleteIcon: any;
+    className: any;
+    emptyGuideText: "Choose file!";
+    api: any;
+    disabled: boolean;
+    showFooter: boolean;
 }
 declare function FileUploader(props: FileUploaderProps): JSX.Element;
 declare namespace FileUploader {
     var propTypes: {
         borderWidth: PropTypes.Requireable<number>;
-        max: PropTypes.Requireable<number>;
+        maxCount: PropTypes.Requireable<number>;
+        minCount: PropTypes.Requireable<number>;
         title: PropTypes.Requireable<string>;
         chooseText: PropTypes.Requireable<string>;
         deleteIcon: PropTypes.Requireable<PropTypes.ReactElementLike>;
         className: PropTypes.Requireable<any>;
         emptyGuideText: PropTypes.Requireable<string>;
+        api: PropTypes.Requireable<(...args: any[]) => any>;
+        disabled: PropTypes.Requireable<boolean>;
+        showFooter: PropTypes.Requireable<boolean>;
     };
     var defaultProps: {
         borderWidth: number;
-        max: number;
+        maxCount: number;
+        minCount: number;
         title: string;
         chooseText: string;
         deleteIcon: JSX.Element;
         className: string;
         emptyGuideText: string;
+        disabled: boolean;
+        showFooter: boolean;
     };
 }
 export default FileUploader;
--- src/FileUploader.tsx
+++ src/FileUploader.tsx
@@ -1,47 +1,71 @@
 import * as React from 'react';
 import { DeleteForever } from "@material-ui/icons";
 import * as PropTypes from "prop-types";
-import { Box, Typography, Button, List, ListItem, ListItemText, ListItemIcon } from "@material-ui/core";
+import {
+    Box,
+    Badge,
+    Typography,
+    Button,
+    List,
+    ListItem,
+    ListItemText,
+    ListItemSecondaryAction,
+    IconButton,
+    CircularProgress
+} from "@material-ui/core";
 
 
 interface FileUploaderProps {
-    borderWidth?: number,
-    max?: number
-    title?: string,
-    chooseText?: "Choose",
-    deleteIcon?: any,
-    className?: any,
-    emptyGuideText?: "Choose file!"
+    borderWidth: number,
+    maxCount: number,
+    minCount: number,
+    title: string,
+    chooseText: "Choose",
+    deleteIcon: any,
+    className: any,
+    emptyGuideText: "Choose file!",
+    api: any,
+    disabled: boolean,
+    showFooter: boolean,
 };
 
 FileUploader.propTypes = {
     borderWidth: PropTypes.number,
-    max: PropTypes.number,
+    maxCount: PropTypes.number,
+    minCount: PropTypes.number,
     title: PropTypes.string,
     chooseText: PropTypes.string,
     deleteIcon: PropTypes.element,
     className: PropTypes.any,
     emptyGuideText: PropTypes.string,
+    api: PropTypes.func,
+    disabled: PropTypes.bool,
+    showFooter: PropTypes.bool,
 };
 
 FileUploader.defaultProps = {
     borderWidth: 1,
-    max: -1,
+    maxCount: 10,
+    minCount: 0,
     title: "Title",
     chooseText: "Choose",
     deleteIcon: <DeleteForever />,
     className: "",
     emptyGuideText: "List is empty!",
+    disabled: false,
+    showFooter: false,
 };
 
 function FileUploader(props: FileUploaderProps) {
 
     const refInput = React.useRef<HTMLInputElement>(null);
 
-    const [list, setList] = React.useState<Array<string>>([]);
+    const [wait, setWait] = React.useState<boolean>(false);
+    const [message, setMessage] = React.useState<string>("")
+    const [list, setList] = React.useState<Array<File>>([]);
 
-    function deleteItem(item: string) {
-        setList(list.filter(element => element !== item));
+    function deleteItem(item: File) {
+        setList(list.filter(element => element.name !== item.name));
     }
 
     function chooseFile() {
@@ -53,44 +77,89 @@ function FileUploader(props: FileUploaderProps) {
 
     }
 
+    function upload() {
+        if (list.length < props.minCount) {
+            setMessage("Choose at least " + list.length.toString() + " items");
+        } else {
+            setWait(true);
+            setMessage("");
+            props.api(list, console.log);
+        }
+    }
+
     return (
         <Box border={props.borderWidth} borderColor="divider" className={props.className} >
-            <Box display="flex" flexDirection="row" p={1} alignItems="center">
+            <Box bgcolor="background.default" display="flex" flexDirection="row" p={1} alignItems="center">
                 <Box flexGrow={1}>
                     <Typography>
                         {props.title}
                     </Typography>
                 </Box>
-                <Button variant="text" onClick={chooseFile}>
+                <Button size="small" variant="text" onClick={chooseFile} disabled={props.disabled || wait || props.maxCount === list.length}>
                     {props.chooseText}
-                    <input
-                        ref={refInput}
-                        onChange={console.log}
-                        type="file"
-                        hidden
-                    />
                 </Button>
             </Box>
-            <Box minHeight={50} overflow="auto" maxHeight={150} borderTop={props.borderWidth} borderColor="divider">
+            <input
+                ref={refInput}
+                type="file"
+                hidden
+                multiple
+                onChange={(event) => {
+                    console.log((event.target).files);
+                    const files = (event.target).files;
+                    if (files !== null) {
+                        setList([...list, files[0]]);
+                    }
+                }}
+            />
+            <Box overflow="auto" height={150} borderTop={props.borderWidth} borderColor="divider">
                 {list.length === 0 ?
-                    <Typography color="textSecondary">
-                        {props.emptyGuideText}
-                    </Typography>
+                    <Box p={1} height="100%" display="flex" alignItems="center" justifyContent="center">
+                        <Typography color="textSecondary" align="center" >
+                            {props.emptyGuideText}
+                        </Typography>
+                    </Box>
                     :
-                    <List>
+                    <List dense>
                         {
-                            list?.map((item, index) => (
-                                <ListItem key={index}>
-                                    <ListItemText primary={item} />
-                                    <ListItemIcon onClick={() => deleteItem(item)}>
-                                        {props.deleteIcon}
-                                    </ListItemIcon>
+                            list?.map((file, index) => (
+                                <ListItem key={index} dense>
+                                    <ListItemText primary={file.name} primaryTypographyProps={{ variant: "caption", ...wait ? { color: "textSecondary" } : { color: "textPrimary" } }} />
+                                    <ListItemSecondaryAction >
+                                        <IconButton size="small" onClick={() => deleteItem(file)} disabled={props.disabled || wait}>
+                                            {props.deleteIcon}
+                                        </IconButton>
+                                    </ListItemSecondaryAction>
                                 </ListItem>
                             ))
                         }
                     </List>
                 }
             </Box>
+            {props.showFooter ?
+                <Box bgcolor="background.default" display="flex" alignItems="center" p={1} borderTop={props.borderWidth} borderColor="divider" >
+                    {
+                        wait ? <CircularProgress size={20} /> : null
+                    }
+                    <Box flexGrow={1}>
+                        <Typography variant="caption">
+                            {message}
+                        </Typography>
+                    </Box>
+                    <Button
+                        size="small"
+                        variant="text"
+                        onClick={upload}
+                        disabled={props.disabled || wait}
+                        endIcon={
+                            <Badge
+                                style={{ marginLeft: 5, marginRight: 5 }}
+                                {...wait ? { color: "default" } : { color: "primary" }}
+                                badgeContent={list.length} />}>
+                        Upload
+                    </Button>
+                </Box>
+                : null}
         </Box>
     );
 }
